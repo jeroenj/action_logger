1fd2dd6022a1e7c0f7ccfd23eb4bc7c8343bf0cf
Jeroen Jacobs <git@jeroenj.be>
2012-10-17 17:02:01 +0200

    Obviously helper related code should be in the helper and not in the main module

--- app/helpers/action_logs_helper.rb
+++ app/helpers/action_logs_helper.rb
@@ -1,5 +1,10 @@
 module ActionLogsHelper
   def log_action action, source, attrs = {}
+    attrs[:sender] || try(:current_user)
+    attrs[:ip] || request.try(:remote_ip)
+    attrs[:data] || params
+    attrs[:admin_id] || try(:current_admin_user)
+
     ActionLogger.log action, source, attrs
   end
 end
--- lib/action_logger.rb
+++ lib/action_logger.rb
@@ -3,11 +3,6 @@ require 'action_logger/engine'
 
 module ActionLogger
   def self.log action, source, attrs = {}
-    attrs[:sender] || try(:current_user)
-    attrs[:ip] || request.try(:remote_ip)
-    attrs[:data] || params
-    attrs[:admin_id] || try(:current_admin_user)
-
     ActionLog.log action, source, attrs[:semder], attrs[:ip], attrs[:data], attrs[:admin_id]
   end
 end
