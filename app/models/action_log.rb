7b6254b43a869348ffb04a360461ddc226d3a538
Jeroen Jacobs <git@jeroenj.be>
2012-10-17 17:02:18 +0200

    Don't log action message to the Rails logs after creating it

--- app/models/action_log.rb
+++ app/models/action_log.rb
@@ -2,7 +2,6 @@ class ActionLog < ActiveRecord::Base
   serialize :data
 
   def self.log action, source, sender = nil, ip = nil, data = {}, admin_id = nil
-    log = create! action: action, source: source, sender_id: sender.try(:id), sender_type: sender.try(:class).try(:name), ip: ip, data: data, admin_id: (admin_id.present? && admin_id.respond_to?(:id) ? admin_id.id : admin_id)
-    Rails.logger.info log.inspect
+    create! action: action, source: source, sender_id: sender.try(:id), sender_type: sender.try(:class).try(:name), ip: ip, data: data, admin_id: (admin_id.present? && admin_id.respond_to?(:id) ? admin_id.id : admin_id)
   end
 end
