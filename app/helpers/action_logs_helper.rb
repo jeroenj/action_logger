80b37544a6edae628750dade92a24f7601333357
Jeroen Jacobs <git@jeroenj.be>
2012-10-18 17:30:06 +0200

    actually set defaults in log_action helper

--- app/helpers/action_logs_helper.rb
+++ app/helpers/action_logs_helper.rb
@@ -1,9 +1,9 @@
 module ActionLogsHelper
   def log_action action, source, attrs = {}
-    attrs[:sender] || try(:current_user)
-    attrs[:ip] || request.try(:remote_ip)
-    attrs[:data] || params
-    attrs[:admin_id] || try(:current_admin_user)
+    attrs[:sender] ||= try(:current_user)
+    attrs[:ip] ||= request.try(:remote_ip)
+    attrs[:data] ||= params
+    attrs[:admin_id] ||= try(:current_admin_user)
 
     ActionLogger.log action, source, attrs
   end
