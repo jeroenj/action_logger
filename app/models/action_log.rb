388024fa2688f89daecdd6fe83bcdd77abf7b31b
Jeroen Jacobs <git@jeroenj.be>
2012-10-18 17:29:56 +0200

    Use ActiveRecord polymorphism to set sender

--- app/models/action_log.rb
+++ app/models/action_log.rb
@@ -1,7 +1,8 @@
 class ActionLog < ActiveRecord::Base
+  belongs_to :sender, polymorphic: true
   serialize :data
 
   def self.log action, source, sender = nil, ip = nil, data = {}, admin_id = nil
-    create! action: action, source: source, sender_id: sender.try(:id), sender_type: sender.try(:class).try(:name), ip: ip, data: data, admin_id: (admin_id.present? && admin_id.respond_to?(:id) ? admin_id.id : admin_id)
+    create! action: action, source: source, sender: sender, ip: ip, data: data, admin_id: (admin_id.present? && admin_id.respond_to?(:id) ? admin_id.id : admin_id)
   end
 end
